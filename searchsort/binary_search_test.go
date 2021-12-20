8dc4ab6c03d29e04f994f2ef75599c92c3b5255b
Süleyman Karakaşoğlu <xtinasfighters@gmail.com>
2021-12-20 19:19:03 +0300

    benchmark methods added to binary search

--- searchsort/binary_search_test.go
+++ searchsort/binary_search_test.go
@@ -32,6 +32,21 @@ func TestBinarySearchWithNonExistingElement(t *testing.T) {
 	}
 }
 
+func benchmarkBinarySearch(arr []int, x int, max int, min int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		BinarySearch(arr, x, max, min)
+	}
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+	arr := []int{2,5,8,12,16,23,38,56,72,91}
+	x := 23
+	max := len(arr) - 1
+	min := 0
+
+	benchmarkBinarySearch(arr, x, max, min, b)
+}
+
 func TestBitwiseBinarySearchWithAnExistingElement(t *testing.T) {
 	arr := []int{2,5,8,12,16,23,38,56,72,91}
 	x := 23
@@ -69,5 +84,18 @@ func TestBitwiseBinarySearch(t *testing.T) {
 	if index != expectedIndex {
 		t.Errorf("Element index is incorrect, got: %d, expected: %v", index, expectedIndex)
 	}
+}
+
+func benchmarkBitwiseBinarySearch(arr []int, x int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		BitwiseBinarySearch(arr, x)
+	}
+}
+
+func BenchmarkBitwiseBinarySearch(b *testing.B) {
+	arr := []int{2,5,8,12,16,23,38,56,72,91}
+	x := 23
+
+	benchmarkBitwiseBinarySearch(arr, x, b)
 }
 
